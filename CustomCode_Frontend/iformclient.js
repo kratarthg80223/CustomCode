3c8ebc7982c59a6cf15d962207440667f2c4b653
kratarthg80223 <155961031+kratarthg80223@users.noreply.github.com>
2024-08-06 17:55:34 +0530

    Append data to file

--- CustomCode_Frontend/iformclient.js
+++ CustomCode_Frontend/iformclient.js
@@ -305,3 +305,7 @@ function onEventRecievedFromMobile(recievedDataString) {
     var dataFromMobileEnd = dataObj["Data"];
     //The above data will be used for funther operations to be performed by form accordingly
 }
+
+function xyz(){
+    console.log("new comment !!!!!")
+}
