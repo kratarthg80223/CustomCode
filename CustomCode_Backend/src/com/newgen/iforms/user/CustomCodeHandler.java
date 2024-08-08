d8a11fa9d4f7dd7c8932a6258459d19c332c3b1e
kratarthg80223 <155961031+kratarthg80223@users.noreply.github.com>
2024-08-08 14:40:44 +0530

    Append data to file

--- CustomCode_Backend/src/com/newgen/iforms/user/CustomCodeHandler.java
+++ CustomCode_Backend/src/com/newgen/iforms/user/CustomCodeHandler.java
@@ -99,7 +99,7 @@ public class CustomCodeHandler extends IFormCustomHooks implements IFormServerEv
     }
     
     @Override
-    public String executeServerEvent(IFormReference ifr, String string, String string1, String string2, String string3) {
+    public String executeServerEvent(IFormReference ifr, String string, String string1, String string2, String string3) { if(ctrlId.equals(id)){ executeServerEvent_button1(); } {
             try {
                 ifr.setValue("textbox10", "great!! way");                
             } catch (Exception e) {
@@ -107,6 +107,17 @@ public class CustomCodeHandler extends IFormCustomHooks implements IFormServerEv
             }
                return "";
 }
+
+public String executeServerEvent_button1(IFormReference ifr, String string, String string1, String string2, String string3) {public String executeServerEvent_button1(IFormReference ifr, String string, String string1, String string2, String string3){
+dsdf
+sdf
+dg
+dfg
+dfg
+fdg
+dfg
+dg
+}}
     
    
     @Override
