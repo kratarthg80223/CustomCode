537924356e08de4b50b3932d8177dcebb329b92c
kratarthg80223 <155961031+kratarthg80223@users.noreply.github.com>
2024-08-08 16:30:53 +0530

    Update CustomCodeHandler.java

--- CustomCode_Backend/src/com/newgen/iforms/user/CustomCodeHandler.java
+++ CustomCode_Backend/src/com/newgen/iforms/user/CustomCodeHandler.java
@@ -100,27 +100,12 @@ public class CustomCodeHandler extends IFormCustomHooks implements IFormServerEv
     
     @Override
     public String executeServerEvent(IFormReference ifr, String string, String string1, String string2, String string3) {
-   if(ctrlId.equals(button1)){
-   executeServerEvent_button1();
- } 
             try {
                 ifr.setValue("textbox10", "great!! way");                
             } catch (Exception e) {
                 ApplicationLogger.writeConsoleLog(null, IFormConstants.DESIGNER_LOGGER_NAME, "Exception in CustomCodeHandler : " + e.getMessage());
             }
                return "";
-}
-
-public String executeServerEvent_button1(IFormReference ifr, String string, String string1, String string2, String string3){
-sdfwer
-defaultwerwe
-fs
-wf
-sg
-erw
-erg
-dfg
-erw
 }
 
    
